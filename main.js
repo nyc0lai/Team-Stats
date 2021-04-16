0d5032cded5a4e03ff9c832b179d7833c41dff91
nyc0lai <nyc0lai@gmail.com>
2021-04-16 22:25:31 +0300

    exercice finish

---
+++ main.js
@@ -0,0 +1,45 @@
+const team = {
+_players: [{firstName:'Jeff', lastName:'Driskel', age: 28},
+{firstName:'Dani', lastName:'Alves', age: 38},
+{firstName:'CristiSergi', lastName:'Roberto', age: 28},],
+
+_games: [{opponent:'Broncos', teamPoints:42, opponentPoints:27},
+{opponent:'USTA', teamPoints:42, opponentPoints:27},
+{opponent:'Bristol', teamPoints:10, opponentPoints:9}],
+
+get players() {
+    return this._players;
+},
+
+get games() {
+    return this._games;
+},
+
+addPlayer(firstName, lastName, age) {
+this.players.push({
+    firstName,
+    lastName,
+    age
+});
+},
+
+addGame(opponent, teamPoints, opponentPoints) {
+    this.games.push({
+        opponent,
+        teamPoints,
+        opponentPoints
+    });
+}
+};
+//add a Players
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+
+//add a Games
+team.addGame('Liverpool', 4, 3);
+team.addGame('FC Barcelona', 3, 0);
+team.addGame('Real Madrid', 6, 1);
+
+console.log(team.players);
+console.log(team.games);
